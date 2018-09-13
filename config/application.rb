4a50f37b4e226dd52413933af67c8d1bdf75ba7f
Jonas <jonjts@gmail.com>
2018-09-12 21:29:51 -0300

    Adicionado timezone.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,13 +8,14 @@ Bundler.require(*Rails.groups)
 
 module MeuNegocio
   class Application < Rails::Application
+    config.time_zone = "Brasilia"
+    config.i18n.default_locale = :'pt-BR'
 
     config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      env_file = File.join(Rails.root, "config", "local_env.yml")
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
       end if File.exists?(env_file)
     end
-    
   end
 end
