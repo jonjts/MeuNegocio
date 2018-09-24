8fdfc2220807825bd7d75faeb5f072de88bce5e5
Jonas <jonjts@gmail.com>
2018-09-23 22:48:58 -0300

    Alteração no layout de empresas.

---
+++ app/helpers/empresas_helper.rb
@@ -0,0 +1,17 @@
+module EmpresasHelper
+  def create_nome_membro(user, empresa)
+    tag = ""
+    if user.id == current_user.id
+      tag.concat(content_tag :b, "(Você) ")
+    end
+    tag.concat(user.name + ", " + user.email + " ")
+    if empresa.sou_admin user
+      tag.concat(content_tag :span, "admin", class: "badge badge-info", title: "Você é adminstrador dessa empresa")
+    end
+    return raw(tag)
+  end
+
+  def membros(empresa)
+    @users = empresa.users.joins(:administradores).order("name, administradores.id")
+  end
+end
--- app/helpers/minhas_empresas_helper.rb
+++ app/helpers/minhas_empresas_helper.rb
@@ -1,10 +1,10 @@
 module MinhasEmpresasHelper
-  def remover_admin(empresa)
+  def remover_admin(empresa, texto)
     @adimins = Administrador.where(empresa_id: empresa.id, user_id: current_user.id)
     if @adimins.blank?
       return ""
     else
-      return link_to fa_icon("lock"), administrador_remover_path(@adimins.first), method: :delete, "data-confirm": "Sair da administração?", class: "btn btn-outline-danger btn-xs", data: {toggle: "tooltip", placement: "top"}, title: "Sair da administração"
+      return link_to fa_icon("lock", text: texto), administrador_remover_path(@adimins.first), method: :delete, "data-confirm": "Sair da administração?", class: "btn btn-outline-danger btn-xs", data: {toggle: "tooltip", placement: "top"}, title: "Sair da administração"
     end
   end
 end
